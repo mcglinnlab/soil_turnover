2fe2d6af663786717676c14dc536b6077a3382e6
Dan McGlinn <danmcglinn@gmail.com>
2015-05-12 22:06:58 -0400

    Add exploratory spatial and temporal analysis.

--- scripts/data_merge.R
+++ scripts/data_merge.R
@@ -16,7 +16,7 @@ head(myc_productivity)
 
 # first rename the fields that we do not wish to merge:
 samp_fields = c("session", "fuid", "tube", "frame", "fulldate", "new_session",
-               "julian1990", "ring", "block", "co2treat", "ntreat", "interval")
+                "julian1990", "ring", "block", "co2treat", "ntreat", "interval")
 prod_fields = c('len.prod', 'len.mort', 'len.stand')
 
 clean_dat = function(df, type) {
@@ -49,7 +49,7 @@ tubecrds = tubecrds[tubecrds$tube != 15, c('tube','tube.x','tube.y')]
 head(tubecrds)
 
 # merge tube coords with productivity ------------------------------------------
-    
+
 prod = merge(prod, tubecrds, by.x = c('tube'), by.y = c('tube'), all=TRUE)
 head(prod)
 dim(prod)
@@ -67,4 +67,28 @@ prod_avg = aggregate(prod[ , c('root.len.prod', 'root.len.mort', 'root.len.stand
 head(prod_avg)
 dim(prod_avg)
 
+# drop records without spatial coordinates
+prod_avg = subset(prod_avg, subset=!is.na(tube.x))
 
+# explore geographic patterns --------------------------------------------
+
+# for time being adjust spatial coords to allow for ring differences
+
+x = (prod_avg$ring - 1) * 1000 + prod_avg$tube.x
+y = (prod_avg$ring - 1) * 1000 + prod_avg$tube.y
+
+gdist = dist(cbind(x, y))
+gdist = ifelse(gdist > 25, 25, gdist)
+plot(density(gdist), ylim=c(0,.02))
+
+tdist = dist(prod_avg$fulldate)
+plot(density(tdist))
+
+root_mso = mso(rda(prod_avg$root.len.prod), prod_avg$fulldate)
+msoplot(root_mso)
+
+root_d = dist(prod_avg$root.len.prod)
+pdf('./figs/root_time.pdf')
+plot(tdist, root_d, type='n')
+lines(lowess(tdist, root_d), col='red', lwd=3)
+dev.off()
