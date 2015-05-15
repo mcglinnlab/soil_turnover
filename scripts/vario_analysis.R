d3fc242e0a1fd0f0a086b1054fa340c52ca6b9fc
Dan McGlinn <danmcglinn@gmail.com>
2015-05-15 09:21:29 -0400

    Improve the exploratory spatio-temporal anslysis.

--- scripts/vario_analysis.R
+++ scripts/vario_analysis.R
@@ -1,28 +1,30 @@
 library(vario)
 
-load('./data/prod.Rdata')
-
-time = prod$fulldate - min(prod$fulldate)
+# import demography data
+demo = read.csv('./data/demo_root_rhiz_myco_avg_across_frames.csv', 
+                colClasses = c(rep('numeric', 17), 'Date'))
+                
+time = demo$fulldate - min(demo$fulldate)
 
 # explore geographic patterns --------------------------------------------
 
 # for time being adjust spatial coords to allow for ring differences
 
-plot(prod$tube.east, prod$tube.north, xlim=c(-100, 300))
-for(i in seq_along(unique(prod$block))) {
-    points(prod$tube.east[prod$block == i], 
-           prod$tube.north[prod$block == i], col=i, pch=19)
+plot(demo$tube.east, demo$tube.north, xlim=c(-100, 300))
+for(i in seq_along(unique(demo$block))) {
+    points(demo$tube.east[demo$block == i], 
+           demo$tube.north[demo$block == i], col=i, pch=19)
 }
 
 # so from a spatial perspective you've really got 3 levels
 # tube < ring < block 
 
-gdist = dist(cbind(prod$tube.east, prod$tube.north))
+gdist = dist(cbind(demo$tube.east, demo$tube.north))
 summary(gdist)
 plot(density(gdist))
 plot(density(log10(gdist+1)))
 
-tdist = dist(prod$fulldate)
+tdist = dist(demo$fulldate)
 summary(tdist)
 plot(density(tdist))
 
@@ -46,10 +48,10 @@ gd = as.dist((as.matrix(dist(g)) ==  0)*1)
 round(dist(y), 2) * gd
 
 breaks = c(1, 30, 60, 120, 240, 480)
-vt_root = vario(prod$root.len.prod, coord = time, hmin=20, hmax=480, 
+vt_root = vario(demo$root.len.prod, coord = time, hmin=20, hmax=480, 
                 breaks=breaks, log=F)
-vt_rhizo = vario(prod$rhiz.len.prod, coord = time, hmax=365)
-vt_myco = vario(prod$myc.prod, coord=time, hmax=365)
+vt_rhizo = vario(demo$rhiz.len.prod, coord = time, hmax=365)
+vt_myco = vario(demo$myc.prod, coord=time, hmax=365)
 
 pdf('./figs/vt_tube.pdf', width=7, height=7*3)
 plot(vt_root$vario$Dist, vt_root$vario$exp.var, ylim=c(0, 10))
@@ -63,10 +65,10 @@ lines(lowess(vt_root$vario$Dist, vt_root$vario$exp.var, f=.05),
       col='red', lwd=2)
 dev.off()
 
-root_mso = mso(rda(prod$root.len.prod), prod$fulldate)
+root_mso = mso(rda(demo$root.len.prod), demo$fulldate)
 msoplot(root_mso)
 
-root_d = dist(prod$root.len.prod)
+root_d = dist(demo$root.len.prod)
 pdf('./figs/root_time.pdf')
 plot(tdist, root_d, type='n')
 lines(lowess(tdist, root_d), col='red', lwd=3)
